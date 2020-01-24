2ce6006c60e914044fda286f483f05f793ab6ecb
Gilles <19607616+GillesDP@users.noreply.github.com>
2020-01-24 19:53:05 +0100

    v1.4

---
+++ src/assets/js/form.js
@@ -0,0 +1,7 @@
+const fName = document.getElementById("fName").parentNode;
+const lName = document.getElementById("lName").parentNode;
+
+fName.style.width = "48%";
+fName.style.display = "inline-block";
+lName.style.width = "48%";
+lName.style.display = "inline-block";
